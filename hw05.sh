f521bc0094d02d1b80fe0ab8b8927262debee78d
Megan Gillen <megangillen@megans-mbp.dhcp.nd.edu>
2019-10-07 21:57:41 -0400

    a few changes

--- hw05.sh
+++ hw05.sh
@@ -1,8 +1,29 @@
 ## Question 1
 
 ## Question 2
-echo gender
+## a.
+echo "gender highest earner"
 awk 'NR==1 {print $1}'
+
+echo "years experienc highest earner"
+awk 'NR==1 {print $2}'
+
+echo "wage highest earner"
+awk 'NR==1 {print $3}
+
+## b.
+echo "gender lowest  earner"
+awk 'NR==3295 {print $1}'
+
+echo "years experience lowest earner"
+awk 'NR==3295 {print $2}'
+
+echo "wage lowest earner"
+awk 'NR==3295 {print $3}
+
+## c.
+echo "number of females in top ten earners"
+cat wages.csv | head -n 10 | grep "female" | wc -l
 
 ## Question 3
 
