d7dbae472b4c89d33210272f28b671b3c28b8637
rosiecrisman <rosiecrisman@rcrismacomputer.dhcp.nd.edu>
2019-10-07 22:39:54 -0400

    answer to question 1

--- hw05.sh
+++ hw05.sh
@@ -1,5 +1,6 @@
-## Question 1
-
+## Question 1 
+cat $1 | tr ',' ' ' | sort --field-separator=' ' --key=$2,$3 | cut -d ' ' -f $2,$3 | grep -v gender | grep -v year
+ 
 ## Question 2
 echo gender
 awk 'NR==1 {print $1}'
