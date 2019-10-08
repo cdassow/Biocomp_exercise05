5216b476b9ae64cfe8443039f06d216ea554aed1
Megan Gillen <megangillen@megans-mbp.dhcp.nd.edu>
2019-10-07 22:37:43 -0400

    another update

--- hw05.sh
+++ hw05.sh
@@ -3,23 +3,23 @@
 ## Question 2
 ## a.
 echo "gender highest earner"
-awk 'NR==1 {print $1}'
+sort -k 4 wages.csv | tr "," " " | awk 'NR==1 {print $1; exit}'
 
 echo "years experienc highest earner"
-awk 'NR==1 {print $2}'
+sort -k 4 wages.csv | tr "," " " | awk 'NR==3295  {print $2}'
 
 echo "wage highest earner"
-awk 'NR==1 {print $3}
+sort -k 4 wages.csv | tr "," " " | awk 'NR==3295 {print $3}
 
 ## b.
-echo "gender lowest  earner"
-awk 'NR==3295 {print $1}'
+echo "gender lowest earner"
+sort -k 4 wages.csv | tr "," " " | awk 'NR==1 {print $1}'
 
 echo "years experience lowest earner"
-awk 'NR==3295 {print $2}'
+sort -k 4 wages.csv | tr "," " " | awk 'NR==1 {print $2}'
 
 echo "wage lowest earner"
-awk 'NR==3295 {print $3}
+sort -k 4 wages.csv | tr "," " " | awk 'NR==1 {print $3}'
 
 ## c.
 echo "number of females in top ten earners"
