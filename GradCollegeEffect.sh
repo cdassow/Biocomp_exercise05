02bcd65ce90c0033ee267f3005298d3d651a3c3e
Faith Taylor <ftaylor@nd.edu>
2019-10-09 19:15:51 -0400

    Some minor deletions

--- GradCollegeEffect.sh
+++
@@ -1,8 +0,0 @@
-# takes the minimum wage earner who completed 12 years of school and compares
-# it to the minimum wage earner who completed 16 years of school
-# usage: bash GradCollegeEffect.sh
-
-val1=$(cat wages.csv | cut -d , -f 2,4| grep -e 12, | sort -n -t, -k2 | cut -d , -f 2 | sort -n | head -1)
-val2=$(cat wages.csv | cut -d , -f 2,4| grep -e 16, | sort -n -t, -k2 | cut -d , -f 2 | sort -n | head -1)
-echo "The difference between the minimum wage earner who graduated college and the minimum wage earner who graduated high school is:"
-echo "$val1 - $val2" | bc
