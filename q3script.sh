8934a9c7dd6f54ffc232b1f89e8e105f7b828a11
Seth Parker <sethparker@abieslasiocarpa.dhcp.nd.edu>
2019-10-09 13:42:09 -0400

    still working on q3. think i'm close. gonna do some googling

--- q3script.sh
+++ q3script.sh
@@ -5,11 +5,11 @@
 #both at once?: cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[26]{1}," | tr , " " | sort -k1,1
 
 #High School Min
-HighSchoolMin =  cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[2]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2
+HighSchoolMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[2]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2)
 
 #College Min
-CollegeMin = cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[6]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2
+CollegeMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[6]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2)
 
-Difference = $CollegeMin - $HighSchoolMin | bc
+Difference=$($CollegeMin-$HighSchoolMin | bc)
 
 echo The difference in minimum wages between high school and college graduates is $Difference.
