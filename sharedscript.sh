d2b4038363cb2f02ff6aa03e8b1ef7f6d8e86044
aphubert <ahubert@nd.edu>
2019-10-09 17:00:26 -0400

    This should work

--- sharedscript.sh
+++ sharedscript.sh
@@ -1,13 +1,17 @@
 #Anna's part
 #using the variable $1 as the wages.csv textfile
-cat $1 | cut -d "," -f 1,2 | tr "," " " | sort -n -k 2 | sort -u > uniquewages.csv
+cat $1 | cut -d "," -f 1,2 | grep -v "gender" | tr "," " " | sort -u -k1,1 -k2,2g > uniquewages.txt
 
 #Clare's part
 #using the variable $1 as the wages.csv file
-cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 > highestwages.csv
-cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -k3 > lowestwages.csv
-cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 | grep -w female | head -10 > toptenfemales.csv
-cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 |head -10 | grep -c female > numberfemalesintopten.csv
+echo "Highest Wages"
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 
+echo "Lowest Wages"
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -k3
+echo "Top Ten Females"  
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 | grep -w female | head -10 
+echo "Number of Females in Top Ten"
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 |head -10 | grep -c female
 
 
 
