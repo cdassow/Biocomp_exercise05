914a12afa0462bdf0566937609f578fd53d93c48
Clare Pierret <cpierret@nd.edu>
2019-10-09 16:36:45 -0400

    Clare_AssignQ2

--- sharedscript.sh
+++ sharedscript.sh
@@ -1,5 +1,13 @@
 #Anna's part
-#using the variable $1 as the wages.txt textfile
-cat $1 | cut -d "," -f 1,2 | tr "," " " | sort -n -k 2 | sort -u > uniquewages.txt
+#using the variable $1 as the wages.csv textfile
+cat $1 | cut -d "," -f 1,2 | tr "," " " | sort -n -k 2 | sort -u > uniquewages.csv
 
 #Clare's part
+#using the variable $1 as the wages.csv file
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 > highestwages.csv
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -k3 > lowestwages.csv
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 | grep -w female | head -10 > toptenfemales.csv
+cat $1 | cut -d "," -f 1,2,4 | tr "," " " | sort -r -k3 |head -10 | grep -c female > numberfemalesintopten.csv
+
+
+
