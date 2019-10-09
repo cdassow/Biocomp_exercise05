f90b9c584a8d6532f6f6b6f0e68abb3a944fcf07
Grace Petrosini <gpetrosi@nd.edu>
2019-10-09 19:36:09 -0400

    Working thorugh Assignment 03

--- graduationEffect.sh
+++ graduationEffect.sh
@@ -1,5 +1,15 @@
 #For Exercise 05 Assignment 3. This script determines the effect of graduating college on the minimum wage for earners in the wages.csv dataset
-#USAGE: 
+#USAGE: bash graduationEffect.sh filename
+
+#high school
+cut -d , -f 3-4 "$1" | grep -E "1[2-5]" > highschool.csv
+hsWageAvg = avg col 4 highschoo.csv
+
+#college
+cut -d , -f 3-4 "$1" | grep -E "1[6-]" > college.csv
+collegeWageAvg = avg col 4 college.csv
+
+echo "$hsWageAvg - $collegeWageAvg" | bc
 
 #cp columns 3 and 4, separate high school graduates (3rd column, 12=<16) and college graduates (3rd column, 16=<) into separate csv files maybe outside the script
 #take average of 4th column for hs and college
