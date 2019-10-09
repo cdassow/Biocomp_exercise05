e67af5a46c96b973d5a42396247e18dc81d38a77
Grace Petrosini <gpetrosi@nd.edu>
2019-10-09 19:14:09 -0400

    Assignment 01 v2, beginning of Assignment 03

--- genderExp.sh
+++ genderExp.sh
@@ -1,3 +1,4 @@
-file=wages.csv
-delimiter=','
-cut -d "$delimiter" -f 1-2 "$file" | tr "," " " > genderYrsExp.csv
+#Exercise 05 Assignment 1. sort by gender and experience
+#USAGE: bash genderExp.sh filename deliminator
+
+cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k2 -t"$2" | sort -k1 -n -t"$2"
---
+++ graduationEffect.sh
@@ -0,0 +1,6 @@
+#For Exercise 05 Assignment 3. This script determines the effect of graduating college on the minimum wage for earners in the wages.csv dataset
+#USAGE: 
+
+#cp columns 3 and 4, separate high school graduates (3rd column, 12=<16) and college graduates (3rd column, 16=<) into separate csv files maybe outside the script
+#take average of 4th column for hs and college
+#subtract college_avg_wage - hs_avg_wage = final answer
