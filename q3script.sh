7c6178f1b656f757801fd741d32943da89da7080
Seth Parker <sethparker@abieslasiocarpa.dhcp.nd.edu>
2019-10-09 14:46:39 -0400

    QUESTION 3 COMPLETED

--- q3script.sh
+++ q3script.sh
@@ -1,8 +1,6 @@
-#working on q3
+#QUESTION 3 COMPLETE
 #USAGE: bash q3script.sh
 
-
-#both at once?: cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[26]{1}," | tr , " " | sort -k1,1
 
 #High School Min
 HighSchoolMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[2]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2)
@@ -10,6 +8,6 @@ HighSchoolMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[2]{1}," | tr 
 #College Min
 CollegeMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[6]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2)
 
-Difference=$($CollegeMin-$HighSchoolMin | bc)
+Difference=$(echo "$CollegeMin-$HighSchoolMin" | bc)
 
 echo The difference in minimum wages between high school and college graduates is $Difference.
