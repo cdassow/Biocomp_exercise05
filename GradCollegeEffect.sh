fa3c0e62d74e5c36139d3e0e8c544d04d46ab9ab
Mary Solokas <msolokas@nd.edu>
2019-10-09 19:12:56 -0400

    All parts have been added, plus a fun extra code

--- GradCollegeEffect.sh
+++ GradCollegeEffect.sh
@@ -1,5 +1,8 @@
-# effect of graduating college on minimum wage for earners
+# takes the minimum wage earner who completed 12 years of school and compares
+# it to the minimum wage earner who completed 16 years of school
 # usage: bash GradCollegeEffect.sh
 
-val1 = (cat wages.csv | cut -d , -f 2,4| grep -e 12, | sort -n -t, -k2 | cut -d , -f 2)
-
+val1=$(cat wages.csv | cut -d , -f 2,4| grep -e 12, | sort -n -t, -k2 | cut -d , -f 2 | sort -n | head -1)
+val2=$(cat wages.csv | cut -d , -f 2,4| grep -e 16, | sort -n -t, -k2 | cut -d , -f 2 | sort -n | head -1)
+echo "The difference between the minimum wage earner who graduated college and the minimum wage earner who graduated high school is:"
+echo "$val1 - $val2" | bc
