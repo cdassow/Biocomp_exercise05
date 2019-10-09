825a3067cb19db83535a3f238d041c9538a2813d
Grace Petrosini <gpetrosi@nd.edu>
2019-10-09 19:22:11 -0400

    Assignment 01 v3

--- genderExp.sh
+++ genderExp.sh
@@ -1,4 +1,4 @@
 #Exercise 05 Assignment 1. sort by gender and experience
 #USAGE: bash genderExp.sh filename deliminator
 
-cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k2 -t"$2" | sort -k1 -n -t"$2"
+cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k1 -t"$2" | sort -k2 -t"$2" -n
