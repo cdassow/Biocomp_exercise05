b81c79295b9a8bf9206583ce0eca667ee6a0f429
Kurt Kohler <kkohler3@nd.edu>
2019-10-09 19:36:56 -0400

    someprogress_on_q2

--- q2script.sh
+++ q2script.sh
@@ -1,5 +1,13 @@
-#script for the biocomp exercise. 
+#script for the biocomp exercise.
 #usage: bash script.sh 'prolly some variables'
+# top earner
+cat wages.csv | cut -d "," -f 1,2,4 | tr "," " " | sort -k 3 -r | head -n 2
+#lowest earner
+wages.csv | cut -d "," -f 1,2,4 | tr "," " " | sort -k 3 -r | tail -n 1
+
+#top 10
+cat wages.csv | cut -d "," -f 1,2,4 | tr "," " " | sort -k 3 -r | head -n 11 | tail -n 10 | cut -d " " -f 1
+
 
 for in
 do
