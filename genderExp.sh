8707d123886aea2d8b35226d68fb8b0377b059a3
stschaeffer21 <sschaef@DESKTOP-NUDCN9N.localdomain>
2019-10-10 13:05:26 -0400

    added part 2 and made part 1 go to a file

--- genderExp.sh
+++ genderExp.sh
@@ -1,4 +1,8 @@
 #Exercise 05 Assignment 1. sort by gender and experience
-#USAGE: bash genderExp.sh filename deliminator
+#USAGE: bash genderExp.sh filename delimitor
 
-cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k1 -t"$2" | sort -k2 -t"$2" -n
+cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k1 -t"$2" | sort -k2 -t"$2" -n > newwage.csv
+cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 1
+cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | tail -n 2 | head -n 1
+cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 10 | grep female | wc -l
+
