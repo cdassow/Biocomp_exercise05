1f97d629b7a72fc2dfc42f12d7efecf811aa4cd4
stschaeffer21 <sschaef@DESKTOP-NUDCN9N.localdomain>
2019-10-10 14:33:31 -0400

    finished part 3

--- genderExp.sh
+++ genderExp.sh
@@ -5,4 +5,7 @@ cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k1 -t"$2" | sort -k2 -t"$2" -n > ne
 cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 1
 cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | tail -n 2 | head -n 1
 cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 10 | grep female | wc -l
-
+a=$(cut wages.csv -d , -f 3,4 | grep -E "16," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
+b=$(cut wages.csv -d , -f 3,4 | grep -E "12," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
+echo "$a - $b" | bc
+ 
