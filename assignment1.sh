90696cbf62e2255d82cc6e25e48975ac2d8244b4
allymarrs <amarrs@nd.edu>
2019-10-10 18:08:25 -0400

    removed seperate scripts for clarity

--- assignment1.sh
+++
@@ -1,4 +0,0 @@
-#write a file with each uniqe gender/yearsExperience combo in wages.csv
-#usage: bash assignment1.sh
-
-cat wages.csv | cut -d , -f 1,2 | grep [mf] | tr ',' ' ' | sort -k1,1 -k2n | uniq
