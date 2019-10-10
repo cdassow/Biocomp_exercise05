3705336ee24b554befcd12c7d828f686d50a35ab
Grace Petrosini <gpetrosi@nd.edu>
2019-10-10 13:28:09 -0400

    Working through assignment 2

--- genderExp.sh
+++ genderExp.sh
@@ -1,8 +1,11 @@
 #Exercise 05 Assignment 1 and 2
 #USAGE: bash genderExp.sh filename delimitor
 
+#Assignment 1
 cut -d "$2" -f 1-2 "$1" | tr "," " " | sort -k1 -t"$2" | sort -k2 -t"$2" -n > newwage.csv
-cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 1
-cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | tail -n 2 | head -n 1
-cut wages.csv -d , -f 1,2,4 | sort -t , -k 3 -n -r | head -n 10 | grep female | wc -l
+
+#Assignment 2
+cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | head -n 1
+cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | tail -n 2 | head -n 1
+cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | head -n 10 | grep female | wc -l
 
