6c3b5423c81d42ee25574778f60c800f4dfb9c71
stschaeffer21 <sschaef@DESKTOP-NUDCN9N.localdomain>
2019-10-10 16:57:25 -0400

    all done

--- genderExp.sh
+++ genderExp.sh
@@ -2,17 +2,17 @@
 #USAGE: bash genderExp.sh filename delimitor
 
 #Assignment 1
-cut -d $2 -f 1-2 "$1" | sort -t $2 -u | sort -k1 -n | sort -k2 -n | tr "$2" " " > newwages.csv
+cut -d , -f 1-2 wages.csv | grep -v gender | sort -u | tr ',' ' ' | sort -k1,1d -k2,2n > newwages.csv
 #Assignment 2
 echo Highest Earner
-cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | head -n 1
+cut -d , -f 1,2,4 wages.csv | sort -t , -k 3 -n -r | head -n 1
 echo Lowest Earner
-cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | tail -n 2 | head -n 1
+cut -d , -f 1,2,4 wages.csv | sort -t , -k 3 -n -r | tail -n 2 | head -n 1
 echo Number of Women in Top Ten Earners
-cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | head -n 10 | grep female | wc -l
+cut -d , -f 1,2,4 wages.csv | sort -t , -k 3 -n -r | head -n 10 | grep female | wc -l
 
 #Assignment 3
-a=$(cut -d $2 -f 3,4 "$1" | grep -E "16," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
-b=$(cut -d $2 -f 3,4 "$1" | grep -E "12," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
+a=$(cut -d , -f 3,4 wages.csv | grep -E "16," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
+b=$(cut -d , -f 3,4 wages.csv | grep -E "12," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
 echo Difference in Wages between HS and College Graduates
 echo "$a - $b" | bc
