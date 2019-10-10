a048df922c2590bd6819957d0ebd73e89dea4a0f
Elise Paietta <epaietta@nd.edu>
2019-10-10 17:48:28 -0400

    fixed question 2

--- wagesscript.sh
+++ wagesscript.sh
@@ -1,20 +1,18 @@
-#i dont know how to put it in a shell script but here this might be good for the first part ?
-# also dont know if we can put multiple for loops within one shell script
-for lines in wages.csv
-do
-cat $lines | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.csv
-done
+#QUESTION 1
 
-for i in wages.csv
-do
-cat $i | sort -n -r | head -n 1 
+cat wages.csv | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.cvs 
 
-#here is the script for the first part of question 2
+
+
+#QUESTION 2
+echo "Part  1"
 cat wages.csv | sort –n –t’,’ –k4 –r | head –n 1
-#here is the script for the second part of question 2 but have to figure out 
-#how to take off the column headers maybe just grep whatever the gender is of this person
-
-cat wages.csv | sort –n –t’,’ –k4 | head –n 2
-
-#here is the third part of question 2
+echo "Part 2"
+cat wages.csv | sort –n –t’,’ –k4 | head -n 2 | tail -n 1
+echo "Part 3"
 cat wages.csv | grep female | sort –n –t’,’ –k4 –r | head –n 10
+
+
+
+#QUESTION 3
+echo "$val1 - $val2" | bc
