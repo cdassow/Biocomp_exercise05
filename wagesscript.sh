94e0044feb19c58e1bd3e74566f5548c8990d335
Daniel <dguobadi@nd.edu>
2019-10-10 18:57:46 -0400

    some changes

--- wagesscript.sh
+++ wagesscript.sh
@@ -1,16 +1,16 @@
 #QUESTION 1
 
-cat wages.csv | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.cvs 
+cat wages.csv | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.csv 
 
 
 
 #QUESTION 2
 echo "Part  1"
-cat wages.csv | sort –n –t’,’ –k4 –r | head –n 1
+cat wages.csv | sort -n -t , -k 4 -r | head -n 1
 echo "Part 2"
-cat wages.csv | sort –n –t’,’ –k4 | head -n 2 | tail -n 1
+cat wages.csv | sort -n -t , -k 4 | head -n 2 | tail -n 1
 echo "Part 3"
-cat wages.csv | grep female | sort –n –t’,’ –k4 –r | head –n 10
+cat wages.csv | grep female | sort -n -t , -k 4 -r | head -n 10
 
 
 
