3fd915b48022ae962bcb79baf1f5167e305cb96f
Kurt Kohler <kkohler3@nd.edu>
2019-10-10 14:37:35 -0400

    Q3 shows less errors but still ^M error I cant get rid of on windows 10 ubuntu

---
+++ q3_3.sh
@@ -0,0 +1,26 @@
+#QUESTION 3 COMPLETE
+
+#USAGE: bash q3script.sh
+
+
+
+
+
+#High School Min
+
+HighSchoolMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[2]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2 | tr \r \n)
+
+
+
+#College Min
+
+CollegeMin=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^[1]{1}[6]{1}," | tr , " " | sort -k2,2r | tail -n 1 | cut -d " " -f 2 | tr \r \n)
+
+
+
+Difference=$(echo "$CollegeMin-$HighSchoolMin" | bc)
+
+
+
+echo The difference in minimum wages between high school and college graduates is $Difference.
+
