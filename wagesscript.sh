ddab1ea407e28176c9409d25f1f66f9e89b9c842
Daniel <dguobadi@nd.edu>
2019-10-10 19:49:11 -0400

    finally

--- wagesscript.sh
+++ wagesscript.sh
@@ -1,19 +1,12 @@
 #QUESTION 1
-
-cat wages.csv | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.csv 
-
-
-
+cat wages.csv | cut -d , -f 1,2 | sort -n | tr ',' ' ' | uniq > wagesuniq.csv
 #QUESTION 2
-echo "Part  1"
-cat wages.csv | sort -n -t , -k 4 -r | head -n 1
+echo "Part 1"
+cat wages.csv | sort -n -t , -k 4 -r | head -n 1; 
 echo "Part 2"
-cat wages.csv | sort -n -t , -k 4 | head -n 2 | tail -n 1
+cat wages.csv | sort -n -t , -k 4 | head -n 2 | tail -n 1;
 echo "Part 3"
-cat wages.csv | grep female | sort -n -t , -k 4 -r | head -n 10
-
-
-
+cat wages.csv | grep female | sort -n -t , -k 4 -r | head -n 10; 
 #QUESTION 3
 #get the columns of yearsExperience and wage separated and the yearsExperience we want
 val1=$(cat wages.csv | cut -d , -f 2,4 | grep "^12," | head -n 1)
