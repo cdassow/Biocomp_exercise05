8738841acde2cec2c2e4bbc87247d66b2f6d9065
Grace Petrosini <gpetrosi@nd.edu>
2019-10-10 15:28:28 -0400

    Part 3 done; to do: review pt1

--- genderExp.sh
+++ genderExp.sh
@@ -13,7 +13,7 @@ echo Number of Women in Top Ten Earners
 cut -d "$2" -f 1,2,4 "$1" | sort -t "$2" -k 3 -n -r | head -n 10 | grep female | wc -l
 
 #Assignment 3
-a=$(cut "$1" -d $2 -f 3,4 | grep -E "16," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
-b=$(cut "$1" -d $2 -f 3,4 | grep -E "12," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
-echo Differnece in Wages between HS and College Graduates
+a=$(cut -d $2 -f 3,4 "$1" | grep -E "16," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
+b=$(cut -d $2 -f 3,4 "$1" | grep -E "12," | sort -t $2 -k 2 -n | head -n 1 | cut -d $2 -f 2)
+echo Difference in Wages between HS and College Graduates
 echo "$a - $b" | bc
