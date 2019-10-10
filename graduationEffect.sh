edbcf0b36185aef2898a83b0f822f5bdd9e28bb4
Grace Petrosini <gpetrosi@nd.edu>
2019-10-10 13:41:13 -0400

    need to figure out how to set pipline outputs to variables

--- graduationEffect.sh
+++ graduationEffect.sh
@@ -1,16 +1,10 @@
 #For Exercise 05 Assignment 3. This script determines the effect of graduating college on the minimum wage for earners in the wages.csv dataset
 #USAGE: bash graduationEffect.sh filename
 
-#high school
-cut -d , -f 3-4 "$1" | grep -E "1[2-5]" > highschool.csv
-countHS = echo wc -l highschool.csv
-sumHS = 
-hsWageAvg = $sumHS/$countHS | bc
+echo "high school"
+hs = cut -d , -f 3-4 "$1" | grep -E "1[2-5]" | sort -t , -k 2 -n | cut -d , -f 2 | tail -n 1
 
-#college
-cut -d , -f 3-4 "$1" | grep -E "1[6-]" > college.csv
-countC= echo wc -l college.csv
-sumC=
-collegeWageAvg = $sumC/$countC | bc
+echo college
+college = cut -d , -f 3-4 "$1" | grep -E "1[6-]" | sort -t , -k 2 -n | cut -d , -f 2 | tail -n 1
 
-echo "$hsWageAvg - $collegeWageAvg" | bc
+echo "$hs - $college" | bc
