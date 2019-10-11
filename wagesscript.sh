850f1f56585676363d5d5bec2f5ad13a976194cd
Daniel <dguobadi@nd.edu>
2019-10-10 20:15:22 -0400

    final

--- wagesscript.sh
+++ wagesscript.sh
@@ -8,7 +8,6 @@ cat wages.csv | sort -n -t , -k 4 | cut -d , -f 1,2,4 | head -n 2 | tail -n 1;
 echo "Part 3"
 cat wages.csv | sort -n -t , -k 4 -r | head -n 10 | grep female | wc -l;
 #QUESTION 3
-#get the columns of yearsExperience and wage separated and the yearsExperience we want
-val1=$(cat wages.csv | cut -d , -f 2,4 | grep "^12," | head -n 1)
-val2=$(cat wages.csv | cut -d , -f 2,4 | grep "^16," | head -n 1)
-echo "$val1 - $val2" | bc
+val1=$(cat wages.csv | cut -d , -f 2,4 | grep "^12," | cut -d , -f 2 | head -n 1)
+val2=$(cat wages.csv | cut -d , -f 2,4 | grep "^16," | cut -d , -f 2 | head -n 1)
+echo "$val1 - $val2" 
