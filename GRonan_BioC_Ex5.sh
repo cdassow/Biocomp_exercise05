70e24cf8f921a44a9b64b9e9a1471e8c5e817c37
George Ronan <gronan@nd.edu>
2019-10-10 20:58:12 -0400

    George Ronan - Final Version - Commented

--- GRonan_BioC_Ex5.sh
+++ GRonan_BioC_Ex5.sh
@@ -8,21 +8,29 @@
 # Usage: bash Ex5_codeS.sh
 
 ## Part 1 ----------------------------------------------------------------------------------------------------------
-cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | tr ',' ' ' | cut -d ' ' -f 1,2 |
-	sort -k1,1 -k2,2n > output.csv
+cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | # Opens file and removes header (first line)
+	tr ',' ' ' | cut -d ' ' -f 1,2 | # Converts , delimiters to spaces and isolates columns 1 and 2
+	sort -k1,1 -k2,2n > output.csv # Sorts first column alphabetically and sorts second column numerically within
+				       # the same categories, then outputs to output.csv
 
 ## Part 2 ----------------------------------------------------------------------------------------------------------
-for file in wages.csv
-do
-	cat $file | tail -n $(($(wc -l $file | cut -d ' ' -f 1)-1)) | cut -d ',' -f 4 | sort -n > temp.txt
-done
-high=$(cat $file | grep -E $(cat temp.txt | tail -n 1) | cut -d ',' -f 1,2,4)
-low=$(cat $file | grep -E $(cat temp.txt | head -n 1) | cut -d ',' -f 1,2,4)
-fem=0;
+cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | # Opens file and removes header (first line)
+	cut -d ',' -f 4 | sort -n > temp.txt # Isolates fourth column, sorts numerically, then outputs to temporary file
+high=$(cat wages.csv | grep -E $(cat temp.txt | tail -n 1) | # Finds line in wages.csv corresponding to the highest
+							     # value in the temporary file
+	cut -d ',' -f 1,2,4) # Isolates information regarding previously found line (highest earner)
+low=$(cat wages.csv | grep -E $(cat temp.txt | head -n 1) | # Fins line in wages.csv corresponding to the lowest value
+							    # in the temporary file
+	cut -d ',' -f 1,2,4) # Isolates information regarding previously found line (lowest earner)
+fem=0; # Initialize counting variable
 for i in {1..10}
 do
-	fem=$(($fem+$(cat $file | grep -E $(cat temp.txt | tail -n $i | head -n 1) | grep -c -E female)))
+	fem=$(($fem+$(cat wages.csv | grep -E $(cat temp.txt | tail -n $i | head -n 1) | # Isolates single line from
+											 # top 10 earners - found same
+											 # way as above
+	grep -c -E female))) # checks if the line subject is female
 done
+# Outputs
 echo "The highest earner is a $(echo $high | grep -o -E [a-z]+) with " \
 	"$(echo $high | grep -o -E [,][1-9][,] | grep -o -E [^,]) years of experience making " \
 	"$(echo $high | grep -o -E [0-9]+\\.[0-9]+) money"
@@ -30,14 +38,22 @@ echo "The lowest earner is a $(echo $low | grep -o -E [a-z]+) with " \
 	"$(echo $low | grep -o -E [,][1-9][,] | grep -o -E [^,]) years of experience making " \
 	"$(echo $low | grep -o -E [0-9]+\\.[0-9]+) money"
 echo "The number of females among the top 10 most paid is $fem"
-rm temp.txt
+rm temp.txt # Removes temporary files
 
 ## Part 3 ----------------------------------------------------------------------------------------------------------
-cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | cut -d ',' -f 3,4 | grep -E 12[,] > HS.txt
-cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | cut -d ',' -f 3,4 | grep -E 16[,] > CLG.txt
-HS_avg=$(cat HS.txt | cut -d ',' -f 2 | awk '{total = total + $1}END{print total}')
-HS_avg=$(echo "$HS_avg / $(wc -l HS.txt | cut -d ' ' -f 1)" | bc -l)
-CLG_avg=$(cat CLG.txt | cut -d ',' -f 2 | awk '{total = total + $1}END{print total}')
-CLG_avg=$(echo "$CLG_avg / $(wc -l CLG.txt | cut -d ' ' -f 1)" | bc -l)
+cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | # Opens file and removes header (first line)
+	cut -d ',' -f 3,4 | grep -E 12[,] > HS.txt # Isolates school years and wages, then exports subjects with 12
+						   # years of education to HS.txt
+cat wages.csv | tail -n $(($(wc -l wages.csv | cut -d ' ' -f 1)-1)) | # Opens file and removes header (first line)
+	cut -d ',' -f 3,4 | grep -E 16[,] > CLG.txt # Isolates school years and wages, then exports subjects with 16
+						    # years of education to CLG.txt
+HS_avg=$(cat HS.txt | cut -d ',' -f 2 | # Isolates wages from HS.txt
+	awk '{total = total + $1}END{print total}') # Totals wages isolated from HS.txt
+HS_avg=$(echo "$HS_avg / $(wc -l HS.txt | cut -d ' ' -f 1)" | bc -l) # Divides total by the number of lines in HS.txt
+CLG_avg=$(cat CLG.txt | cut -d ',' -f 2 | # Isolates wages from CLG.txt
+	awk '{total = total + $1}END{print total}') # Totals wages isolated from CLG.txt
+CLG_avg=$(echo "$CLG_avg / $(wc -l CLG.txt | cut -d ' ' -f 1)" | bc -l) # Divides total by the number of lines in CLG.txt
+# Outputs
 echo "The average wages without only 12 years of school is $HS_avg"
 echo "The average with 16 years of school is $CLG_avg"
+rm HS.txt CLG.txt # Removes temporary files
