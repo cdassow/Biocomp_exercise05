61dd140efe2211786a9867af2815567101b75d93
katnip <katnip@katnipdembp.dhcp.nd.edu>
2019-10-10 23:34:11 -0400

    task 2 done by collaborator

---
+++ hw5.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Task 1
+cut -d , -f 1,2 wages.csv | sed '1d' | sort -t , -k 1 | sort -t , -n -k 2 | uniq > gender_experience.csv
+
+# Task 2
+echo " the gender, yearsExperience, and wage for the highest earner is "
+sort -t , -k 4 -n -r wages.csv | head -n 1 | cut -d , -f 1,2,4 
+
+echo " the gender, yearsExperience, and wage for the lowest earner is "
+sed '1d' wages.csv | sort -t , -k 4 -n | head -n 1 | cut -d , -f 1,2,4 
+
+echo " the number of females in the top ten earners is "
+sed '1d' wages.csv | sort -t , -k 4 -n -r | head -n 10 | grep female | wc -l
+
