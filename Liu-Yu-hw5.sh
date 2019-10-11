c8dffa0607a4c770a89fc1ec0fd3282f486d5706
katnip <katnip@katnipdembp.dhcp.nd.edu>
2019-10-11 00:28:32 -0400

    final modification

--- Liu-Yu-hw5.sh
+++ Liu-Yu-hw5.sh
@@ -14,4 +14,9 @@ echo " the number of females in the top ten earners is "
 sed '1d' wages.csv | sort -t , -k 4 -n -r | head -n 10 | grep female | wc -l
 
 # Task 3
+echo "the effect of graduating college on the minimum wage is"
+val1=$(cut -d, -f 3,4 wages.csv | grep '12,' | sort -t , -k 2 | head -1 | cut -d , -f 2)
+val2=$(cut -d, -f 3,4 wages.csv | grep '16,' | sort -t , -k 2 | head -1 | cut -d , -f 2)
+
+echo "$val1 - $val2" | bc
 
